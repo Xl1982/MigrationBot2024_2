c679ba3fbc945d0c73f1a60b4d537a18ef2ed8ec
Legan <legan.16@rambler.ru>
2023-07-26 15:52:06 +0300

    фикс выбора

--- source/group_chat/sending_messages/send_text_messages.py
+++ source/group_chat/sending_messages/send_text_messages.py
@@ -3,9 +3,8 @@ import pytz
 import asyncio
 
 from source.data.classes.messages import TextMessagesStorage
-from .text_message import message, send_bot_message
 from source.bot_init import dp, bot
-from .config_chat import config_chat, times_to_send
+from .config_chat import config_chat
 from source.logger_bot import logger
 
 
@@ -14,14 +13,14 @@ async def send_text_messages():
     storage = TextMessagesStorage(r'source\data\messages.json')
 
     chat_id = config_chat['chat_id']
-    # current_day = datetime.datetime.now(pytz.timezone('Europe/Madrid')).strftime('%A').capitalize()
-    current_day = datetime.datetime.now().strftime('%A').capitalize()
+    current_day = datetime.datetime.now(pytz.timezone('Europe/Madrid')).strftime('%A').capitalize()
+    # current_day = datetime.datetime.now().strftime('%A').capitalize()
 
     logger.info('Включение функции для отправки текстовых сообщений')
 
     while config_chat['text_message']:
-        # now = datetime.datetime.now(pytz.timezone('Europe/Madrid')).time()
-        now = datetime.datetime.now().time()
+        now = datetime.datetime.now(pytz.timezone('Europe/Madrid')).time()
+        # now = datetime.datetime.now().time()
         # Получаем словарь для текущего дня
         messages_for_current_day = storage.get_messages_for_day(current_day)
 
